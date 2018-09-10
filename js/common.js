4bd383651f65db3113548ac55fdeeae776451614
Johnynoxwell <jimboo19898@gmail.com>
2018-09-10 11:41:11 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -38,7 +38,15 @@ $(function () {
 	
 	
 	
-	
+	$(".jsscroll").click(function() {
+		$("html, body").animate({
+			 scrollTop: $($(this).attr("href")).offset().top + "px"
+		}, {
+			 duration: 500,
+			 easing: "swing"
+		});
+		return false;
+ });
 	
 	
 	
